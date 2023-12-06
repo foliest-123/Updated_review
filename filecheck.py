40dec10f02d0902c8bf678d76d6d90d97c741e83
foilest-123 <thegreatvijay1991@gmail.com>
2023-12-06 14:29:23 +0530

    first commit

--- filecheck.py
+++ filecheck.py
@@ -10,18 +10,13 @@ modified_files = re.findall(r'\s+modified:\s+([\w./]+\.ndjson)', result.stdout)
 print("Modified .ndjson files:")
 for file in modified_files:
     print(file)
-
-    diff_result = subprocess.run(['git', 'diff', file], capture_output=True, text=True)
-    
-
-changes = subprocess.run(
-    f'git diff --staged --color=always {file} | Select-String -Pattern "\\e[32m|\\e[0m"',
-    capture_output=True,
-    text=True,
-    shell=True
-)
-
-changed_lines = changes.stdout
-
-print("Changed lines:")
-print(len(changed_lines))
+    try:
+        diff_result = subprocess.run(
+        f'git diff --staged --color=always {file} | Select-String -Pattern "`e[32m|`e[0m"',
+        text=True,
+        shell=False,
+        stdout=True
+        )
+        print(diff_result.stdout)
+    except Exception as e:
+        print("Error..." , e)
