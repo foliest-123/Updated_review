568c4cad1a69f1f694d543b54a5b9b300da8346b
foilest-123 <thegreatvijay1991@gmail.com>
2023-12-06 16:44:43 +0530

    ok

--- filecheck.py
+++ filecheck.py
@@ -4,19 +4,19 @@ import re
 subprocess.run(['git', 'add', '.'])
 
 result = subprocess.run(['git', 'status'], capture_output=True, text=True)
-
 modified_files = re.findall(r'\s+modified:\s+([\w./]+\.ndjson)', result.stdout)
 
-print("Modified .ndjson files:")
+
 for file in modified_files:
     print(file)
-    try:
-        diff_result = subprocess.run(
-        f'git diff --staged --color=always {file} | Select-String -Pattern "`e[32m|`e[0m"',
-        text=True,
-        shell=False,
-        stdout=True
-        )
-        print(diff_result.stdout)
-    except Exception as e:
-        print("Error..." , e)
+
+    diff_command = (f'git diff --color=always {file}')
+
+    diff_result = subprocess.run(
+        diff_command,
+        capture_output=True,
+        shell=True,
+        text=True)
+
+    print(diff_result.stdout)
+    print(diff_result.stderr)
